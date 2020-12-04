4c58b36f00f3b328fce50364d1a81869297f6d9c
Michal Drbohlav <m.drbohlav1@gmail.com>
2020-12-04 09:21:53 +0100

    updated read input function

--- read_input.js
+++ read_input.js
@@ -4,10 +4,18 @@ function defaultMapper(line) {
   return line.trim();
 }
 
-function readInput(fileName, mapper = defaultMapper) {
-  const input = fs.readFileSync(fileName, 'utf8');
-  
-  return input.split('\n').filter(line => line).map(mapper);
+function readInput(fileName, options = {}) {
+  let input = fs.readFileSync(fileName, 'utf8').split('\n');
+
+  if (!options.includeEmptyLines) {
+    input = input.filter(line => line);
+  }
+
+  if (!options.mapper) {
+    options.mapper = defaultMapper;
+  }
+
+  return input.map(options.mapper);
 }
 
 module.exports = readInput;
